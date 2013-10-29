98bf8451d85debb2651818a2cd30f50d8aaeb072
Pete Brown <rendhalver@gmail.com>
2013-10-29 15:06:40 +1000

    adding environment directories

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,4 +24,25 @@ class puppet-master {
         mode => 600,
     }
 
+	file { "/etc/puppet/environments":
+		owner => "puppet",
+		group => "puppet",
+		mode => 755,
+	}
+	
+	file { "/etc/puppet/environments/production":
+		owner => "puppet",
+		group => "puppet",
+		mode => 755,
+		require => File["/etc/puppet/environments"],
+	}
+	
+	file { "/etc/puppet/environments/development":
+		owner => "puppet",
+		group => "puppet",
+		mode => 755,
+		require => File["/etc/puppet/environments"],
+	}
+
+	# crons for updating the puppet module trees
 }
