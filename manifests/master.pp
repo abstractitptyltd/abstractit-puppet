140416d5f37440ab468e7f4f070e6c1bff7fcdd4
Pete Brown <rendhalver@gmail.com>
2013-10-30 13:38:10 +1000

    adding librarian-puppet config for development environment adding manifests for development

--- manifests/master.pp
+++ manifests/master.pp
@@ -25,23 +25,78 @@ class puppet::master {
     }
 
 	file { "/etc/puppet/environments":
+		ensure => directory,
 		owner => "puppet",
 		group => "puppet",
 		mode => 755,
 	}
-	
+/*
 	file { "/etc/puppet/environments/production":
+		ensure => directory,
 		owner => "puppet",
 		group => "puppet",
 		mode => 755,
 		require => File["/etc/puppet/environments"],
 	}
 	
+	file { "/etc/puppet/environments/production/manifests":
+		ensure => directory,
+		owner => "puppet",
+		group => "puppet",
+		mode => 755,
+		require => File["/etc/puppet/environments/production"],
+	}
+	
+	file { "/etc/puppet/environments/production/Puppetfile":
+		ensure => file,
+		owner => "puppet",
+		group => "puppet",
+		mode => 640,
+		content => template("puppet/production/Puppetfile.erb"),
+		require => File["/etc/puppet/environments/production"],
+	}
+*/
 	file { "/etc/puppet/environments/development":
+		ensure => directory,
 		owner => "puppet",
 		group => "puppet",
 		mode => 755,
 		require => File["/etc/puppet/environments"],
+	}
+
+	file { "/etc/puppet/environments/development/Puppetfile":
+		ensure => file,
+		owner => "puppet",
+		group => "puppet",
+		mode => 644,
+		content => template("puppet/development/Puppetfile.erb"),
+		require => File["/etc/puppet/environments/development"],
+	}
+
+	file { "/etc/puppet/environments/development/manifests":
+		ensure => directory,
+		owner => "puppet",
+		group => "puppet",
+		mode => 755,
+		require => File["/etc/puppet/environments/development"],
+	}
+
+	file { "/etc/puppet/environments/development/manifests/site.pp":
+		ensure => file,
+		owner => "puppet",
+		group => "puppet",
+		mode => 644,
+		content => template("puppet/development/site.pp.erb"),
+		require => File["/etc/puppet/environments/development/manifests"],
+	}
+
+	file { "/etc/puppet/environments/development/manifests/nodes.pp":
+		ensure => file,
+		owner => "puppet",
+		group => "puppet",
+		mode => 600,
+		content => template("puppet/development/nodes.pp.erb"),
+		require => File["/etc/puppet/environments/development/manifests"],
 	}
 
 	# crons for updating the puppet module trees
