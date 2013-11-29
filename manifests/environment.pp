00ce477fd931aa3d555615c2177596c3ef25cdbc
Pete Brown <rendhalver@gmail.com>
2013-11-29 10:23:53 +1000

    Indentation fixes

--- manifests/environment.pp
+++ manifests/environment.pp
@@ -1,86 +1,86 @@
 define puppet::environment (
-	$branch = $name,
-	$librarian = true,
-	$cron_minutes = "0,15,30,45",
-	$user = 'puppet',
-	$group = 'puppet',
+  $branch = $name,
+  $librarian = true,
+  $cron_minutes = '0,15,30,45',
+  $user = 'puppet',
+  $group = 'puppet',
 ) {
-	## sets up the files for each environment
+  ## sets up the files for each environment
 
-	file { "/etc/puppet/environments/${name}":
-		ensure => directory,
-		owner => $user,
-		group => $group,
-		mode => 755,
-		require => File["/etc/puppet/environments"],
-	}
-	
-	file { "/etc/puppet/environments/${name}/Puppetfile":
-		ensure => file,
-		owner => $user,
-		group => $group,
-		mode => 640,
-		content => template("puppet/Puppetfile.erb"),
-    #		content => template("puppet/${name}/Puppetfile.erb"),
-		require => File["/etc/puppet/environments/${name}"],
-	}
+  file { "/etc/puppet/environments/${name}":
+    ensure  => directory,
+    owner   => $user,
+    group   => $group,
+    mode    => '0755',
+    require => File['/etc/puppet/environments'],
+  }
 
-	file { "/etc/puppet/environments/${name}/Puppetfile.lock":
-		ensure => file,
-		owner => $user,
-		group => $group,
-		mode => 644,
-		require => File["/etc/puppet/environments/${name}"],
-	}
+  file { "/etc/puppet/environments/${name}/Puppetfile":
+    ensure   => file,
+    owner    => $user,
+    group    => $group,
+    mode     => '0640',
+    content  => template('puppet/Puppetfile.erb'),
+    #content => template("puppet/${name}/Puppetfile.erb"),
+    require  => File["/etc/puppet/environments/${name}"],
+  }
 
-	file { "/etc/puppet/environments/${name}/manifests":
-		ensure => directory,
-		owner => 'puppet',
-		group => 'puppet',
-		mode => 755,
-		require => File["/etc/puppet/environments/${name}"],
-	}
-	
-	file { "/etc/puppet/environments/${name}/manifests/site.pp":
-		ensure => file,
-		owner => 'puppet',
-		group => 'puppet',
-		mode => 644,
-		content => template("puppet/${name}/site.pp.erb"),
-		require => File["/etc/puppet/environments/${name}/manifests"],
-	}
+  file { "/etc/puppet/environments/${name}/Puppetfile.lock":
+    ensure  => file,
+    owner   => $user,
+    group   => $group,
+    mode    => '0644',
+    require => File["/etc/puppet/environments/${name}"],
+  }
 
-	gitclone::clone { "manifest_includes_${name}":
-		real_name  => "includes",
-		localtree => "/etc/puppet/environments/${name}/manifests",
-		source    => "https://bitbucket.org/pivitptyltd/puppet-manifest-includes.git",
-		branch    => $name,
-	}
-	gitclone::pull { "manifest_includes_${name}":
-		real_name => "includes",
-		localtree => "/etc/puppet/environments/${name}/manifests",
-		require   => Gitclone::Clone["manifest_includes_${name}"],
-	}
+  file { "/etc/puppet/environments/${name}/manifests":
+    ensure  => directory,
+    owner   => 'puppet',
+    group   => 'puppet',
+    mode    => '0755',
+    require => File["/etc/puppet/environments/${name}"],
+  }
 
-	file { "/etc/puppet/environments/${name}/manifests/nodes.pp":
-		ensure  => file,
-		owner   => 'puppet',
-		group   => 'puppet',
-		mode    => 600,
-		content => template("puppet/${name}/nodes.pp.erb"),
+  file { "/etc/puppet/environments/${name}/manifests/site.pp":
+    ensure  => file,
+    owner   => 'puppet',
+    group   => 'puppet',
+    mode    => '0644',
+    content => template("puppet/${name}/site.pp.erb"),
+    require => File["/etc/puppet/environments/${name}/manifests"],
+  }
+
+  gitclone::clone { "manifest_includes_${name}":
+    real_name => 'includes',
+    localtree => "/etc/puppet/environments/${name}/manifests",
+    source    => 'https://bitbucket.org/pivitptyltd/puppet-manifest-includes.git',
+    branch    => $name,
+  }
+  gitclone::pull { "manifest_includes_${name}":
+    real_name => 'includes',
+    localtree => "/etc/puppet/environments/${name}/manifests",
+    require   => Gitclone::Clone["manifest_includes_${name}"],
+  }
+
+  file { "/etc/puppet/environments/${name}/manifests/nodes.pp":
+    ensure  => file,
+    owner   => 'puppet',
+    group   => 'puppet',
+    mode    => '0600',
+    content => template("puppet/${name}/nodes.pp.erb"),
     require => Gitclone::Pull["manifest_includes_${name}"],
     #require => File["/etc/puppet/environments/${name}/manifests"],
-	}
+    }
 
-	# cron for updating the ${name} puppet module trees
+    # cron for updating the ${name} puppet module trees
     cron_job { "puppet_modules_${name}":
-    	enable			=> $librarian,
-        interval        => "d",
-        script          => "# created by puppet
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+      enable   => $librarian,
+      interval => 'd',
+      script   => "# created by puppet
+      PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-${cron_minutes} * * * * ${user} cd /etc/puppet/environments/${name} && librarian-puppet update 2>&1
-",
+      ${cron_minutes} * * * * ${user} cd /etc/puppet/environments/${name} && librarian-puppet update 2>&1
+      ",
     }
 
 }
--- manifests/master.pp
+++ manifests/master.pp
@@ -1,86 +1,86 @@
 class puppet::master {
 
-    File {
-        owner => "root",
-        group => "root",
-    }
+  File {
+    owner => 'root',
+    group => 'root',
+  }
 
-    # setup puppetdb
-    class { 'puppetdb':
-      ssl_listen_address => "0.0.0.0",
-    }
-    class { 'puppetdb::master::config':
-      puppet_service_name => "apache2",
-    }
+  # setup puppetdb
+  class { 'puppetdb':
+    ssl_listen_address => '0.0.0.0',
+  }
+  class { 'puppetdb::master::config':
+    puppet_service_name => 'apache2',
+  }
 
-    ## setup hiera
+  ## setup hiera
 
-    file { "/etc/hiera.yaml":
-        ensure  => file,
-        content => template("puppet/hiera.yaml.erb"),
-        mode    => 644,
-        require => Gitclone::Pull["pivit_hieradata"],
-    }
-    file { "/etc/puppet/hiera.yaml":
-        ensure   => link,
-        target   => "/etc/hiera.yaml",
-        require  => File["/etc/hiera.yaml"],
-    }
+  file { '/etc/hiera.yaml':
+    ensure  => file,
+    content => template('puppet/hiera.yaml.erb'),
+    mode    => '0644',
+    require => Gitclone::Pull['pivit_hieradata'],
+  }
+  file { '/etc/puppet/hiera.yaml':
+    ensure   => link,
+    target   => '/etc/hiera.yaml',
+    require  => File['/etc/hiera.yaml'],
+  }
 
-    gitclone::clone { "pivit_hieradata":
-      real_name  => "hieradata",
-      localtree => "/etc/puppet",
-      source    => "https://bitbucket.org/pivitptyltd/puppet-hieradata",
-      branch    => "production",
-    }
-    gitclone::pull { "pivit_hieradata":
-      real_name => "hieradata",
-      localtree => "/etc/puppet",
-      clean     => false,
-      require   => Gitclone::Clone["pivit_hieradata"],
-    }
+  gitclone::clone { 'pivit_hieradata':
+    real_name => 'hieradata',
+    localtree => '/etc/puppet',
+    source    => 'https://bitbucket.org/pivitptyltd/puppet-hieradata',
+    branch    => 'production',
+  }
+  gitclone::pull { 'pivit_hieradata':
+    real_name => 'hieradata',
+    localtree => '/etc/puppet',
+    clean     => false,
+    require   => Gitclone::Clone['pivit_hieradata'],
+  }
 
-    package { "puppetmaster-passenger": 
-        ensure => installed
-    }
+  package { 'puppetmaster-passenger':
+    ensure => installed
+  }
 
-    file { "apache2.monitrc":
-        ensure => file,
-        path => "/etc/monit/conf.d/apache2.monitrc",
-        source => "puppet:///modules/apache2/apache2.monitrc",
-        mode => 644,
-        notify => Service["monit"],
-    }
+  file { 'apache2.monitrc':
+    ensure => file,
+    path   => '/etc/monit/conf.d/apache2.monitrc',
+    source => 'puppet:///modules/apache2/apache2.monitrc',
+    mode   => '0644',
+    notify => Service['monit'],
+  }
 
-    file { "puppet.cfg":
-        ensure => file,
-        path => "/usr/local/backups/puppet.cfg",
-        source => "puppet:///modules/puppet/puppet.cfg",
-        mode => 600,
-    }
+  file { 'puppet.cfg':
+    ensure => file,
+    path   => '/usr/local/backups/puppet.cfg',
+    source => 'puppet:///modules/puppet/puppet.cfg',
+    mode   => '0600',
+  }
 
-	package { "librarian-puppet":
-		ensure => installed,
-		provider => gem,
-	}
+  package { 'librarian-puppet':
+    ensure   => installed,
+    provider => gem,
+  }
 
-	file { "/etc/puppet/environments":
-		ensure => directory,
-		owner => "puppet",
-		group => "puppet",
-		mode => 755,
-	}
+  file { '/etc/puppet/environments':
+    ensure => directory,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0755',
+  }
 
-	puppet::environment { "production":
-		librarian => true,
-	}
+  puppet::environment { 'production':
+    librarian => true,
+  }
 
-	puppet::environment { "development":
-		librarian => false,
-		branch => "master",
-		cron_minutes => "10,25,40,55",
-		user => 'ubuntu',
-		group => 'ubuntu',
-	}
+  puppet::environment { 'development':
+    librarian    => false,
+    branch       => 'master',
+    cron_minutes => '10,25,40,55',
+    user         => 'ubuntu',
+    group        => 'ubuntu',
+  }
 
 }
