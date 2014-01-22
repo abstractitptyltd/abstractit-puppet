7a4b6b0490f0ae56493c156a6e74cb68d9cc0017
Pete Brown <rendhalver@gmail.com>
2014-01-22 14:41:13 +1000

    split facts out into a separate class

--- manifests/client.pp
+++ manifests/client.pp
@@ -2,51 +2,15 @@
 #
 # Setup a puppet client
 #
-# setup facts and puppet agent
+# setup puppet agent
 #
 class puppet::client (
-  $data_centre = '',
-  $mysql_host = '',
-  $ldap_host = '',
-  $wsrep_cluster_name = '',
-  $pub_ipaddress = $::ipaddress,
   $enabled = true,
 ) {
-
-  if ! defined(File['/etc/facter']) {
-    file { '/etc/facter':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'puppet',
-      mode   => '0755',
-    }
-  }
-
-  if ! defined(File['/etc/facter/facts.d']) {
-    file { '/etc/facter/facts.d':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'puppet',
-      mode   => '0755',
-    }
-  }
-
-  file {'/etc/facter/facts.d/local.yaml':
-    ensure    => file,
-    owner     => 'root',
-    group     => 'puppet',
-    mode      => '0640',
-    content   => template('puppet/local_facts.yaml.erb'),
-  }
 
   service { 'puppet':
     ensure => $enabled,
     enable => $enabled,
   }
-
-  ## realise puppet facts
-  File <<| tag == 'puppet_fact_global' |>>
-  File <<| tag == "puppet_fact_${::environment}" |>>
-  File <<| tag == "puppet_fact_${::data_centre}" |>>
 
 }
---
+++ manifests/facts.pp
@@ -0,0 +1,46 @@
+# Class: puppet::facts
+#
+# Setup a puppet facts
+#
+# setup facts
+#
+class puppet::facts (
+  $data_centre = '',
+  $mysql_host = '',
+  $ldap_host = '',
+  $wsrep_cluster_name = '',
+  $pub_ipaddress = $::ipaddress,
+) {
+
+  if ! defined(File['/etc/facter']) {
+    file { '/etc/facter':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'puppet',
+      mode   => '0755',
+    }
+  }
+
+  if ! defined(File['/etc/facter/facts.d']) {
+    file { '/etc/facter/facts.d':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'puppet',
+      mode   => '0755',
+    }
+  }
+
+  file {'/etc/facter/facts.d/local.yaml':
+    ensure    => file,
+    owner     => 'root',
+    group     => 'puppet',
+    mode      => '0640',
+    content   => template('puppet/local_facts.yaml.erb'),
+  }
+
+  ## realise puppet facts
+  File <<| tag == 'puppet_fact_global' |>>
+  File <<| tag == "puppet_fact_${::environment}" |>>
+  File <<| tag == "puppet_fact_${::data_centre}" |>>
+
+}
