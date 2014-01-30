69c123149dabdc640f0b737f7eedf629a331a02f
Pete Brown <rendhalver@gmail.com>
2014-01-30 11:39:15 +1000

    manage puppetmaster vhost with apache::vhost

--- manifests/master.pp
+++ manifests/master.pp
@@ -1,6 +1,9 @@
-class puppet::master {
+class puppet::master (
+  $host = $::servername,
+) {
 
   include site::monit::apache
+  include apache
 
   File {
     owner => 'root',
@@ -13,6 +16,51 @@ class puppet::master {
   }
   class { 'puppetdb::master::config':
     puppet_service_name => 'apache2',
+  }
+  #class { 'puppetboard':
+  #}
+  #class { 'puppetboard::apache::conf':
+  #}
+
+  class { 'apache::mod::passenger':
+    passenger_high_performance   => 'On',
+    passenger_max_pool_size      => '12',
+    passenger_pool_idle_time     => '1500',
+    passenger_stat_throttle_rate => '120',
+    rack_autodetect              => 'Off',
+    rails_autodetect             => 'Off',
+  }
+
+  apache::vhost{ 'puppetmaster':
+    docroot           => '/usr/share/puppet/rack/puppetmasterd/public/',
+    docroot_owner     => 'root',
+    docroot_group     => 'root',
+    port              => '8140',
+    ssl               => true,
+    ssl_protocol      => '-ALL +SSLv3 +TLSv1',
+    ssl_cipher        => 'ALL:!ADH:RC4+RSA:+HIGH:+MEDIUM:-LOW:-SSLv2:-EXP',
+    ssl_cert          => "/var/lib/puppet/ssl/certs/${host}.pem",
+    ssl_key           => "/var/lib/puppet/ssl/private_keys/${host}.pem",
+    ssl_chain         => '/var/lib/puppet/ssl/certs/ca.pem',
+    ssl_ca            => '/var/lib/puppet/ssl/certs/ca.pem',
+    ssl_crl           => '/var/lib/puppet/ssl/ca/ca_crl.pem',
+    ssl_verify_client => 'optional',
+    ssl_verify_depth  => '1',
+    ssl_options       => ['+StdEnvVars','+ExportCertData'],
+    rack_base_uris    => ['/'],
+    directories       => [
+      { path          => '/usr/share/puppet/rack/puppetmasterd/',
+        options       => 'None',
+        order         => 'allow,deny',
+        allow         => 'from all',
+      }
+    ],
+    request_headers   => [
+      'unset X-Forwarded-For',
+      'set X-SSL-Subject %{SSL_CLIENT_S_DN}e',
+      'set X-Client-DN %{SSL_CLIENT_S_DN}',
+      'set X-Client-Verify %{SSL_CLIENT_VERIFY}e',
+    ],
   }
 
   ## setup hiera
