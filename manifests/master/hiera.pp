dc771e2e591fb1b25c4d69d3937132374f7e70e2
Pete Brown <pete@abstractit.com.au>
2014-05-19 08:56:01 +1000

    adding eyaml setup for hiera

--- manifests/master/hiera.pp
+++ manifests/master/hiera.pp
@@ -5,6 +5,11 @@ class puppet::master::hiera (
   $env_owner = $puppet::master::params::env_owner,
   $hiera_repo = $puppet::master::params::hiera_repo,
   $git_protocol = $puppet::master::params::git_protocol,
+  $gpg = $puppet::master::params::gpg,
+  $eyaml = $puppet::master::params::eyaml,
+  $hiera_yaml_path = $puppet::master::params::hiera_eyaml_path,
+  $hiera_eyaml_path = $puppet::master::params::hiera_eyaml_path,
+  $hiera_gpg_path = $puppet::master::params::hiera_gpg_path,
 ) inherits puppet::master::params {
 
   $git_user = $git_protocol ? {
@@ -19,6 +24,28 @@ class puppet::master::hiera (
     group  => 'puppet',
     mode   => '0700',
   }
+  # eyaml keys
+  file { '/etc/puppet/keys/eyaml':
+    ensure => directory,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0700',
+  }
+  file { '/etc/puppet/keys/eyaml/private_key.pkcs7.pem':
+    ensure => file,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0600',
+    source => 'puppet:///modules/local/eyaml/private_key.pkcs7.pem',
+  }
+  file { '/etc/puppet/keys/eyaml/public_key.pkcs7.pem':
+    ensure => file,
+    owner  => 'puppet',
+    group  => 'puppet',
+    mode   => '0600',
+    source => 'puppet:///modules/local/eyaml/public_key.pkcs7.pem',
+  }
+
   file { $hieradata_path:
     ensure => directory,
     owner  => $env_owner,
@@ -38,7 +65,7 @@ class puppet::master::hiera (
     target   => '/etc/hiera.yaml',
     require  => File['/etc/hiera.yaml'],
   }
-
+/*
   # need to keep this till I get hiera-gpg working properly
   vcsrepo { '/etc/puppet/hieradata':
     ensure   => latest,
@@ -54,5 +81,5 @@ class puppet::master::hiera (
       Site::User[$env_owner],
     ]
   }
-
+*/
 }
--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -6,6 +6,7 @@ class puppet::master::install (
   $r10k_version = $puppet::master::params::r10k_version,
   $gpgme_version = $puppet::master::params::gpgme_version,
   $hiera_gpg_version = $puppet::master::params::hiera_gpg_version,
+  $hiera_eyaml_version = $puppet::master::params::hiera_eyaml_version,
 ) inherits puppet::master::params {
 
   package { 'puppetmaster-common':
@@ -36,6 +37,10 @@ class puppet::master::install (
   }
   package { 'gpgme':
     ensure   => $gpgme_version,
+    provider => gem,
+  }
+  package { 'hiera-eyaml':
+    ensure   => $hiera_eyaml_version,
     provider => gem,
   }
   package { 'hiera-gpg':
