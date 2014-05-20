73e6d60de6909fe8013a7dfe99c18be49c84698c
Pete Brown <pete@abstractit.com.au>
2014-05-20 10:49:36 +1000

    put this back in puppet master doesn't seem to be coping with env based hiera lookups

--- manifests/master/hiera.pp
+++ manifests/master/hiera.pp
@@ -59,4 +59,20 @@ class puppet::master::hiera (
     require  => File['/etc/hiera.yaml'],
   }
 
+  # need to keep this till I get hiera-gpg working properly
+  vcsrepo { '/etc/puppet/hieradata':
+    ensure   => latest,
+    revision => 'production',
+    provider => git,
+    owner    => $env_owner,
+    group    => $env_owner,
+    user     => 'root',
+    source   => $hiera_repo,
+    require  => [
+      Site::User::Githostkey["${env_owner}_bitbucket.org"],
+      Site::User::Githostkey["${env_owner}_altssh.bitbucket.org"],
+      Site::User[$env_owner],
+    ]
+  }
+
 }
