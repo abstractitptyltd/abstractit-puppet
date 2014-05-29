e87ac95f20806ef0165c470d060867127fe7250c
Pete Brown <pete@abstractit.com.au>
2014-05-29 10:28:23 +1000

    abstract facts out into init class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,20 @@
 class puppet (
-  $enabled     = $puppet::params::enabled,
+  $enabled             = $puppet::params::enabled,
   $puppet_version,
   $hiera_version,
   $facter_version,
-  $server      = $puppet::params::server,
-  $environment = $puppet::params::environment,
-  $dumptype    = $puppet::params::dumptype,
-  $devel_repo  = $puppet::params::devel_repo,
-  $host        = $puppet::params::host,
-  $reports     = $puppet::params::reports,) inherits puppet::params {
+  $data_centre,
+  $mysql_host          = '',
+  $ldap_host           = '',
+  $galera_cluster_name = '',
+  $ldap_cluster_name   = '',
+  $pub_ipaddress       = $::ipaddress,
+  $server              = $puppet::params::server,
+  $environment         = $puppet::params::environment,
+  $dumptype            = $puppet::params::dumptype,
+  $devel_repo          = $puppet::params::devel_repo,
+  $host                = $puppet::params::host,
+  $reports             = $puppet::params::reports,) inherits puppet::params {
   $ensure = $enabled ? {
     default => 'running',
     false   => 'stopped',
@@ -18,10 +24,18 @@ class puppet (
     false   => false,
   }
 
+  class { 'puppet::facts':
+    data_centre         => $data_centre,
+    mysql_host          => $mysql_host,
+    ldap_host           => $ldap_host,
+    galera_cluster_name => $galera_cluster_name,
+    ldap_cluster_name   => $ldap_cluster_name,
+    pub_ipaddress       => $pub_ipaddress,
+  }
+
   class { 'puppet::repo':
     devel_repo => $devel_repo,
   } ->
-  class { 'puppet::facts': } ->
   class { 'puppet::install':
     puppet_version => $puppet_version,
     hiera_version  => $hiera_version,
@@ -30,12 +44,13 @@ class puppet (
   class { 'puppet::config':
     server      => $server,
     environment => $environment,
-  } ->
+  } ~>
   class { 'puppet::agent':
-    enable => $enable,
     ensure => $ensure,
+    enable => $enable,
   }
 
   class { 'puppet::backup':
   }
+
 }
--- manifests/master.pp
+++ manifests/master.pp
@@ -1,22 +1,20 @@
 class puppet::master (
-  $puppetdb = $puppet::master::params::puppetdb,
-  $puppetboard = $puppet::master::params::puppetboard,
-) inherits puppet::master::params {
-
-  class{'puppet':} ->
-  class{'puppet::master::install':} ->
-  class{'puppet::master::modules':} ->
-  class{'puppet::master::config':} ->
-  class{'puppet::master::hiera':} ->
-  class{'puppet::master::passenger':} ->
-  class{'puppet::master::backup':} ->
+  $puppetdb    = $puppet::master::params::puppetdb,
+  $puppetboard = $puppet::master::params::puppetboard,) inherits puppet::master::params {
+  class { 'puppet::master::install': } ->
+  class { 'puppet::master::modules': } ->
+  class { 'puppet::master::config': } ->
+  class { 'puppet::master::hiera': } ->
+  class { 'puppet::master::passenger': } ->
+  class { 'puppet::master::backup': } ->
   Class['puppet::master']
 
-  if ( $puppetdb == true ) {
-    class{'puppet::master::puppetdb':}
+  if ($puppetdb == true) {
+    class { 'puppet::master::puppetdb': }
   }
-  if ( $puppetboard == true ) {
-    class{'puppet::master::puppetboard':}
+
+  if ($puppetboard == true) {
+    class { 'puppet::master::puppetboard': }
   }
 
 }
