d930fdd02935ad88152d0c29fd9ed2a437e4b8b0
Pete Brown <pete@abstractit.com.au>
2014-05-30 13:11:38 +1000

    remove templatedir setting

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,11 +1,9 @@
-## class puppet::config
+# # class puppet::config
 # config for puppet agent
 
 class puppet::config (
-  $server = $puppet::params::server,
-  $environment = $puppet::params::environment,
-) inherits puppet::params {
-
+  $server      = $puppet::params::server,
+  $environment = $puppet::params::environment,) inherits puppet::params {
   ini_setting { 'puppet client server':
     ensure  => present,
     path    => "${::settings::confdir}/puppet.conf",
@@ -23,20 +21,4 @@ class puppet::config (
     value   => $environment,
     require => Class['puppet::install'],
   }
-
-  # check our puppet version to see if we need to remove old settings
-  if (versioncmp($puppet_version,'3.5') >= 0) {
-    $deprecated_settings = 'absent'
-  } else {
-    $deprecated_settings = 'present'
-  }
-  # deprecated in puppet 3.5 so remove these settings
-  ini_setting { 'template_dir':
-    ensure  => $deprecated_settings,
-    path    => "${::settings::confdir}/puppet.conf",
-    section => 'main',
-    setting => 'templatedir',
-    value   => '$confdir/templates',
-  }
-
 }
