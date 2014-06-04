1e5a22b64c3a7cae613fcd2d1e77802290924ed0
Pete Brown <pete@abstractit.com.au>
2014-06-04 12:06:31 +1000

    setup apache class properly and don't use base settings or hiera vars don't use default mods for apache switch to worker mpm

--- manifests/master/passenger.pp
+++ manifests/master/passenger.pp
@@ -1,14 +1,16 @@
-## Class puppet::master::passenger
+# Class puppet::master::passenger
 
 class puppet::master::passenger (
-  $passenger_max_pool_size = $puppet::master::params::passenger_max_pool_size,
-  $passenger_pool_idle_time = $puppet::master::params::passenger_pool_idle_time,
+  $passenger_max_pool_size      = $puppet::master::params::passenger_max_pool_size,
+  $passenger_pool_idle_time     = $puppet::master::params::passenger_pool_idle_time,
   $passenger_stat_throttle_rate = $puppet::master::params::passenger_stat_throttle_rate,
-  $passenger_max_requests = $puppet::master::params::passenger_max_requests,
-  $host = $puppet::master::params::host,
-) inherits puppet::master::params {
-
-  include ::apache
+  $passenger_max_requests       = $puppet::master::params::passenger_max_requests,
+  $host = $puppet::master::params::host,) inherits puppet::master::params {
+  class { '::apache':
+    default_vhost => false,
+    serveradmin   => 'webmaster@pivit.net.au',
+    default_mods  => false,
+  }
 
   monit::process { 'puppetmaster':
     host     => '127.0.0.1',
@@ -27,8 +29,8 @@ class puppet::master::passenger (
     rails_autodetect             => 'Off',
   }
 
-  ## puppetmaster vhost in apache
-  apache::vhost{ $host:
+  # # puppetmaster vhost in apache
+  apache::vhost { $host:
     docroot           => '/usr/share/puppet/rack/puppetmasterd/public/',
     docroot_owner     => 'root',
     docroot_group     => 'root',
@@ -44,24 +46,23 @@ class puppet::master::passenger (
     ssl_certs_dir     => '/var/lib/puppet/ssl/certs',
     ssl_verify_client => 'optional',
     ssl_verify_depth  => '1',
-    ssl_options       => ['+StdEnvVars','+ExportCertData'],
+    ssl_options       => [
+      '+StdEnvVars',
+      '+ExportCertData'],
     rack_base_uris    => ['/'],
-    directories       => [
-      { path          => '/usr/share/puppet/rack/puppetmasterd/',
-        options       => 'None',
-        order         => 'allow,deny',
-        allow         => 'from all',
+    directories       => [{
+        path    => '/usr/share/puppet/rack/puppetmasterd/',
+        options => 'None',
+        order   => 'allow,deny',
+        allow   => 'from all',
       }
-    ],
+      ],
     request_headers   => [
       'unset X-Forwarded-For',
       'set X-SSL-Subject %{SSL_CLIENT_S_DN}e',
       'set X-Client-DN %{SSL_CLIENT_S_DN}e',
       'set X-Client-Verify %{SSL_CLIENT_VERIFY}e',
-    ],
-    require           => [
-      Class['puppet::master::install'],
-    ]
+      ],
+    require           => Class['puppet::master::install']
   }
-
 }
