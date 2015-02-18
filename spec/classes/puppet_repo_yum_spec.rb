a6c3cb9f0beaf1108274c861acddaa3f22a536bd
Pete Brown <pete@abstractit.com.au>
2015-02-18 10:40:08 +1000

    replace tabs with spaces

--- spec/classes/puppet_repo_yum_spec.rb
+++ spec/classes/puppet_repo_yum_spec.rb
@@ -99,7 +99,6 @@ describe 'puppet::repo::yum', :type => :class do
       context 'when ::puppet has default parameters' do
         let(:pre_condition){"class{'::puppet':}"}
         it 'should add the puppetlabs-products yum repo' do
-#          pending 'Working on it'
           case facts['operatingsystemmajrelease']
           when '5'
             should contain_yumrepo('puppetlabs-products').with({
@@ -128,7 +127,6 @@ describe 'puppet::repo::yum', :type => :class do
           end
         end
         it 'should add the puppetlabs-deps yum repo' do
-#          pending 'Working on it'
           case facts['operatingsystemmajrelease']
           when '5'
             should contain_yumrepo('puppetlabs-deps').with({
@@ -157,7 +155,6 @@ describe 'puppet::repo::yum', :type => :class do
           end
         end
         it 'should disable the puppetlabs-devel yum repo' do
-#          pending 'Working on it'
           case facts['operatingsystemmajrelease']
           when '5'
             should contain_yumrepo('puppetlabs-devel').with({
@@ -189,7 +186,6 @@ describe 'puppet::repo::yum', :type => :class do
       context 'when ::puppet::manage_repos is false' do
         let(:pre_condition){"class{'::puppet': manage_repos => false}"}
         it 'should not lay down any yum repos' do
-#          pending 'Working on it'
           should_not contain_yumrepo('puppetlabs-products')
           should_not contain_yumrepo('puppetlabs-deps')
           should_not contain_yumrepo('puppetlabs-devel')
@@ -199,7 +195,6 @@ describe 'puppet::repo::yum', :type => :class do
         context 'when ::puppet::devel_repo is false' do
           let(:pre_condition){"class{'::puppet': devel_repo => false}"}
           it 'should disable the puppetlabs-devel yum repo' do
-#            pending 'Working on it'
             case facts['operatingsystemmajrelease']
             when '5'
               should contain_yumrepo('puppetlabs-devel').with({
@@ -230,7 +225,6 @@ describe 'puppet::repo::yum', :type => :class do
           context 'and ::puppet::enable_devel_repo is true' do
             let(:pre_condition){"class{'::puppet': devel_repo => false, enable_devel_repo => true }"}
             it 'should enable the puppetlabs_devel yum repo' do
-#              pending 'Working on it'
               case facts['operatingsystemmajrelease']
               when '5'
                 should contain_yumrepo('puppetlabs-devel').with({
@@ -263,7 +257,6 @@ describe 'puppet::repo::yum', :type => :class do
         context 'when ::puppet::devel_repo is true' do
           let(:pre_condition){"class{'::puppet': devel_repo => true}"}
           it 'should enable the puppetlabs_devel yum repo' do
-#            pending 'Working on it'
             case facts['operatingsystemmajrelease']
             when '5'
               should contain_yumrepo('puppetlabs-devel').with({
@@ -294,7 +287,6 @@ describe 'puppet::repo::yum', :type => :class do
           context 'and ::puppet::enable_devel_repo is false' do
             let(:pre_condition){"class{'::puppet': devel_repo => true, enable_devel_repo => false}"}
              it 'should behave the same' do
-#               pending 'Working on it'
                case facts['operatingsystemmajrelease']
                when '5'
                  should contain_yumrepo('puppetlabs-devel').with({
