7b7dc7dacbc0aa756fb288dea7959fc284e8b88c
Pete Brown <pete@abstractit.com.au>
2015-02-18 12:44:43 +1000

    initial support for puppet::install on RedHat

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,35 +1,40 @@
 # install client packages
 
 class puppet::install (
-  ) {
+) {
   include ::puppet
+  include ::puppet::defaults
+
   $facter_version = $::puppet::facter_version
   $hiera_version  = $::puppet::hiera_version
   $puppet_version = $::puppet::puppet_version
-  # Input validation
-  validate_string(
-    $facter_version,
-    $hiera_version,
-    $puppet_version,
-  )
-  package { 'puppetlabs-release': ensure => latest, }
 
-  package { 'puppet-common':
-    ensure  => $puppet_version,
-    require => Package['puppetlabs-release']
+  $puppet_pkgs    = $::puppet::defaults::puppet_pkgs
+
+  # Input validation
+  validate_string($facter_version, $hiera_version, $puppet_version,)
+
+  validate_array($puppet_pkgs)
+
+  package { 'puppetlabs-release':
+    ensure => latest,
   }
 
-  package { 'puppet':
+  package { $puppet_pkgs:
     ensure  => $puppet_version,
     require => [
-      Package['puppet-common'],
+      Package['puppetlabs-release'],
       Package['hiera'],
       Package['facter'],
       ]
   }
 
-  package { 'hiera': ensure => $hiera_version, }
+  package { 'hiera':
+    ensure => $hiera_version,
+  }
 
-  package { 'facter': ensure => $facter_version, }
+  package { 'facter':
+    ensure => $facter_version,
+  }
 
 }
