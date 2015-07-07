476f73bca3900417c942318ac8674c61fb7a2fb7
Pete Brown <pete@abstractit.com.au>
2015-07-08 09:23:41 +1000

    fixing tests for ca so it checks the setting as well as the value indentation cleanup

--- spec/classes/puppet_master_config_spec.rb
+++ spec/classes/puppet_master_config_spec.rb
@@ -43,197 +43,51 @@ describe 'puppet::master::config', :type => :class do
       context "when fed no parameters" do
         it "should properly set the environmentpath in #{confdir}/puppet.conf" do
           should contain_ini_setting('Puppet environmentpath').with({
-            'ensure'=>'present',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'main',
-            'setting'=>'environmentpath',
-            'value'=>"#{codedir}/environments"
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'main',
+            'setting' => 'environmentpath',
+            'value'   => "#{codedir}/environments"
           })
         end
         it 'should properly set the basemodulepath' do
           should contain_ini_setting('Puppet basemodulepath').with({
-            'ensure'=>'present',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'main',
-            'setting'=>'basemodulepath',
-            'value'=>"#{basemodulepath}"
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'main',
+            'setting' => 'basemodulepath',
+            'value'   => "#{basemodulepath}"
           })
         end
         it 'should disable autosign' do
           should contain_ini_setting('autosign').with({
-            'ensure'=>'absent',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'master',
-            'setting'=>'autosign',
-            'value'=>true
+            'ensure'  => 'absent',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'master',
+            'setting' => 'autosign',
+            'value'   => true
           })
         end
         it 'should disable the future parser' do
           should contain_ini_setting('master parser').with({
-            'ensure'=>'absent',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'master',
-            'setting'=>'parser',
-            'value'=>'future'
+            'ensure'  => 'absent',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'master',
+            'setting' => 'parser',
+            'value'   => 'future'
           })
         end
         it 'should create the \'puppet clean reports\' cronjob' do
           should contain_cron('puppet clean reports').with({
-            :name=>"puppet clean reports",
-            :command=>"cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
-            :user=>"root",
-            :hour=>"21",
-            :minute=>"22",
-            :weekday=>"0"
+            'name'    => "puppet clean reports",
+            'command' => "cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
+            'user'    => "root",
+            'hour'    => "21",
+            'minute'  => "22",
+            'weekday' => "0"
           })
         end
       end#no params
-
-      context 'when ::puppet::ca_server is set and this is not the ca_server' do
-        let(:pre_condition){"class{'::puppet': ca_server => 'bogon.domain.com'}"}
-        let(:facts) do
-          facts.merge({
-            :concat_basedir => '/tmp',
-            :fqdn => 'notbogon.domain.com',
-            :puppetversion => Puppet.version
-          })
-        end
-        it 'should set ca to false' do
-          should contain_ini_setting('puppet CA').with({
-            'ensure'  => 'present',
-            'path'    => "#{confdir}/puppet.conf",
-            'section' => 'master',
-            'value'   => false
-          })
-        end
-      end# end not ca_server
-
-      context 'when ::puppet::ca_server is set and this is the ca_server' do
-        let(:pre_condition){"class{'::puppet': ca_server => 'bogon.domain.com'}"}
-        let(:facts) do
-          facts.merge({
-            :concat_basedir => '/tmp',
-            :fqdn => 'bogon.domain.com',
-            :puppetversion => Puppet.version
-          })
-        end
-        it 'should set ca to false' do
-          should contain_ini_setting('puppet CA').with({
-            'ensure'  => 'present',
-            'path'    => "#{confdir}/puppet.conf",
-            'section' => 'master',
-            'value'   => true
-          })
-        end
-      end# end ca_server
-
-      context 'when report_age has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': report_age => '33'}"}
-        it 'should create the \'puppet clean reports\' cronjob with custom mtime' do
-          should contain_cron('puppet clean reports').with({
-            :name=>"puppet clean reports",
-            :command=>"cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +33 -print0 | xargs -0 -n50 /bin/rm -f",
-            :user=>"root",
-            :hour=>"21",
-            :minute=>"22",
-            :weekday=>"0"
-          })
-        end
-      end #end custom report_age
-
-      context 'when report_clean_hour has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': report_clean_hour => '12'}"}
-        it 'should create the \'puppet clean reports\' cronjob with custom hour' do
-          should contain_cron('puppet clean reports').with({
-            :name=>"puppet clean reports",
-            :command=>"cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
-            :user=>"root",
-            :hour=>"12",
-            :minute=>"22",
-            :weekday=>"0"
-          })
-        end
-      end #end custom report_clean_hour
-
-      context 'when report_clean_min has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': report_clean_min => '59'}"}
-        it 'should create the \'puppet clean reports\' cronjob with custom minute' do
-          should contain_cron('puppet clean reports').with({
-            :name=>"puppet clean reports",
-            :command=>"cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
-            :user=>"root",
-            :hour=>"21",
-            :minute=>"59",
-            :weekday=>"0"
-          })
-        end
-      end #end custom report_clean_min
-
-      context 'when report_clean_weekday has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': report_clean_weekday => '7'}"}
-        it 'should create the \'puppet clean reports\' cronjob with custom weekday' do
-          should contain_cron('puppet clean reports').with({
-            :name=>"puppet clean reports",
-            :command=>"cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
-            :user=>"root",
-            :hour=>"21",
-            :minute=>"22",
-            :weekday=>"7"
-          })
-        end
-      end #end custom report_clean_weekday
-
-      context 'when the $::puppet::master::environment_timeout variable has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': environment_timeout => 'BOGON'}"}
-        it 'should update the environment_timeout via an ini_setting' do
-          should contain_ini_setting('Puppet environment_timeout').with({
-            'ensure'=>'present',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'main',
-            'setting'=>'environment_timeout',
-            'value'=>'BOGON'
-          })
-        end
-      end # environment_timeout
-
-      context 'when the $::puppet::master::environmentpath variable has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': environmentpath => '/BOGON'}"}
-        it 'should update the environmentpath via an ini_setting' do
-          should contain_ini_setting('Puppet environmentpath').with({
-            'ensure'=>'present',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'main',
-            'setting'=>'environmentpath',
-            'value'=>'/BOGON'
-          })
-        end
-      end # environmentpath
-
-      context 'when the $::puppet::master::basemodulepath variable has a custom value' do
-        let(:pre_condition) {"class{'::puppet::master': basemodulepath => '/BOGON:/BOGON2'}"}
-        it 'should update the basemodulepath via an ini_setting' do
-          should contain_ini_setting('Puppet basemodulepath').with({
-            'ensure'=>'present',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'main',
-            'setting'=>'basemodulepath',
-            'value'=>'/BOGON:/BOGON2'
-          })
-        end
-      end # basemodulepath
-
-      context 'when the $::puppet::master::future_parser variable is true' do
-        let(:pre_condition) {"class{'::puppet::master': future_parser => true}"}
-        it 'should update the autosign param via an ini_setting' do
-          should contain_ini_setting('master parser').with({
-            'ensure'=>'present',
-            'path'=>"#{confdir}/puppet.conf",
-            'section'=>'master',
-            'setting'=>'parser',
-            'value'=>'future'
-          })
-        end
-      end # future_parser
 
       context 'when the $::puppet::master::autosign variable is true' do
         context 'and the environment is production' do
@@ -273,6 +127,155 @@ describe 'puppet::master::config', :type => :class do
           end
         end#autosign true in other environemtns
       end## autosign true
+
+      context 'when ::puppet::ca_server is set and this is not the ca_server' do
+        let(:pre_condition){"class{'::puppet': ca_server => 'bogon.domain.com'}"}
+        let(:facts) do
+          facts.merge({
+            :concat_basedir => '/tmp',
+            :fqdn => 'notbogon.domain.com',
+            :puppetversion => Puppet.version
+          })
+        end
+        it 'should set ca to false' do
+          should contain_ini_setting('puppet CA').with({
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'master',
+            'setting' => 'ca',
+            'value'   => false
+          })
+        end
+      end# end not ca_server
+
+      context 'when ::puppet::ca_server is set and this is the ca_server' do
+        let(:pre_condition){"class{'::puppet': ca_server => 'bogon.domain.com'}"}
+        let(:facts) do
+          facts.merge({
+            :concat_basedir => '/tmp',
+            :fqdn           => 'bogon.domain.com',
+            :puppetversion  => Puppet.version
+          })
+        end
+        it 'should set ca to false' do
+          should contain_ini_setting('puppet CA').with({
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'master',
+            'setting' => 'ca',
+            'value'   => true
+          })
+        end
+      end# end ca_server
+
+      context 'when report_age has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': report_age => '33'}"}
+        it 'should create the \'puppet clean reports\' cronjob with custom mtime' do
+          should contain_cron('puppet clean reports').with({
+            :name    => "puppet clean reports",
+            :command => "cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +33 -print0 | xargs -0 -n50 /bin/rm -f",
+            :user    => "root",
+            :hour    => "21",
+            :minute  => "22",
+            :weekday => "0"
+          })
+        end
+      end #end custom report_age
+
+      context 'when report_clean_hour has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': report_clean_hour => '12'}"}
+        it 'should create the \'puppet clean reports\' cronjob with custom hour' do
+          should contain_cron('puppet clean reports').with({
+            :name    => "puppet clean reports",
+            :command => "cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
+            :user    => "root",
+            :hour    => "12",
+            :minute  => "22",
+            :weekday => "0"
+          })
+        end
+      end #end custom report_clean_hour
+
+      context 'when report_clean_min has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': report_clean_min => '59'}"}
+        it 'should create the \'puppet clean reports\' cronjob with custom minute' do
+          should contain_cron('puppet clean reports').with({
+            :name    => "puppet clean reports",
+            :command => "cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
+            :user    => "root",
+            :hour    => "21",
+            :minute  => "59",
+            :weekday => "0"
+          })
+        end
+      end #end custom report_clean_min
+
+      context 'when report_clean_weekday has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': report_clean_weekday => '7'}"}
+        it 'should create the \'puppet clean reports\' cronjob with custom weekday' do
+          should contain_cron('puppet clean reports').with({
+            :name    => "puppet clean reports",
+            :command => "cd #{reports_dir} && find . -type f -name \\*.yaml -mtime +7 -print0 | xargs -0 -n50 /bin/rm -f",
+            :user    => "root",
+            :hour    => "21",
+            :minute  => "22",
+            :weekday => "7"
+          })
+        end
+      end #end custom report_clean_weekday
+
+      context 'when the $::puppet::master::environment_timeout variable has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': environment_timeout => 'BOGON'}"}
+        it 'should update the environment_timeout via an ini_setting' do
+          should contain_ini_setting('Puppet environment_timeout').with({
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'main',
+            'setting' => 'environment_timeout',
+            'value'   => 'BOGON'
+          })
+        end
+      end # environment_timeout
+
+      context 'when the $::puppet::master::environmentpath variable has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': environmentpath => '/BOGON'}"}
+        it 'should update the environmentpath via an ini_setting' do
+          should contain_ini_setting('Puppet environmentpath').with({
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'main',
+            'setting' => 'environmentpath',
+            'value'   => '/BOGON'
+          })
+        end
+      end # environmentpath
+
+      context 'when the $::puppet::master::basemodulepath variable has a custom value' do
+        let(:pre_condition) {"class{'::puppet::master': basemodulepath => '/BOGON:/BOGON2'}"}
+        it 'should update the basemodulepath via an ini_setting' do
+          should contain_ini_setting('Puppet basemodulepath').with({
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'main',
+            'setting' => 'basemodulepath',
+            'value'   => '/BOGON:/BOGON2'
+          })
+        end
+      end # basemodulepath
+
+      context 'when the $::puppet::master::future_parser variable is true' do
+        let(:pre_condition) {"class{'::puppet::master': future_parser => true}"}
+        it 'should update the autosign param via an ini_setting' do
+          should contain_ini_setting('master parser').with({
+            'ensure'  => 'present',
+            'path'    => "#{confdir}/puppet.conf",
+            'section' => 'master',
+            'setting' => 'parser',
+            'value'   => 'future'
+          })
+        end
+      end # future_parser
+
     end
   end
 end
