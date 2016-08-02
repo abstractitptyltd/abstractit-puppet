a45b3e8a6c20ca86674f4755b2d158fbfe8e955d
Mikhail Konyakhin <m.konyahin@gmail.com>
2016-08-02 12:50:51 +0300

    Add validation type of custom_facts parameter and expand warning message.

--- manifests/facts.pp
+++ manifests/facts.pp
@@ -16,6 +16,17 @@ class puppet::facts (
     validate_hash($custom_facts)
   }
 
+  if ( versioncmp($::puppetversion, '4.0.0') >= 0 ) {
+    $custom_facts.each |$custom_fact, $custom_fact_value| {
+      unless is_string($custom_fact_value) or is_array($custom_fact_value) or is_hash($custom_fact_value) {
+        warning("Value of custom fact '${custom_fact}' must be Sring or Array or Hash!")
+      }
+    }
+  } else {
+    $custom_facts_values = values($custom_facts)
+    ::puppet::facts::validation { $custom_facts_values: }
+  }
+
   file { "${facterbasepath}/facts.d/local.yaml":
     ensure  => file,
     owner   => 'root',
