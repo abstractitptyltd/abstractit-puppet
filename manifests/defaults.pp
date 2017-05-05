23a95df3dc254fcd78a9df753125f25441bb2ded
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-05-05 19:35:37 +0200

    update puppet::defaults::{facterbasepath,reports_dir}
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -37,8 +37,8 @@ class puppet::defaults {
         'datadir' => '/etc/puppetlabs/code/hieradata/%{environment}'
       }
     }
-    $facterbasepath            = '/opt/puppetlabs/facter'
-    $reports_dir               = '/opt/puppetlabs/server/data/reports'
+    $facterbasepath            = '/etc/puppetlabs/facter'
+    $reports_dir               = '/etc/puppetlabs/server/data/reports'
     $terminus_package          = 'puppetdb-termini'
     $puppetdb_etcdir           = '/etc/puppetlabs/puppetdb'
     $puppetdb_test_url         = '/pdb/meta/v1/version'
