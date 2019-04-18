ae34282bacd6dc5a40dc1c208ec0ef53a57ae4f2
Michael Simmons <msimmons1989@icloud.com>
2019-04-18 11:51:58 -0400

    add support for most recent apt mod

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -23,7 +23,6 @@ class puppet::repo::apt {
         location   => 'http://apt.puppetlabs.com',
         repos      => $::puppet::collection,
         key        => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
-        key_server => 'pgp.mit.edu',
       }
     } else {
       apt::source { 'puppetlabs':
